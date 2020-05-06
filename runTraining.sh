0dc125979c5404e845d9d22a6650a91c968a0ac6
Jana Schor <hertelj@molsyb047.intranet.ufz.de>
2020-05-06 12:40:00 +0200

    repository cleaned from old shell scripts

--- runTraining.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python deepFPlearn train -i /data/bioinf/projects/data/2019_IDA-chem/deepFPlearn/input/Sun_etal_dataset.csv -o /data/bioinf/projects/data/2019_IDA-chem/deepFPlearn/modeltraining/kfoldCV -t 'smiles' -k 'topological' -s 2048 -a -d 256 -e 2000 -l 0.2 -K 5 -v 2
