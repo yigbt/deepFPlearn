bb69055b7484d4b2022d63205f5bdd973fe7b4a4
Patrick Scheibe <patrick@halirutan.de>
2020-08-06 04:07:07 +0200

    Fix building conda environment
    - Added pytest to list of installed packages
    - Fix file extension typo "yml" vs "yaml"
    - Make creation of the environment yml specification cross-platform
    
    The last point needs some explanation. When we simply export the environment
    without the --from-history option, all packages that are installed in the env will
    be listed. This is not cross-platform because many dependencies depend on the specific
    system where the conda environment is created. What we most like want is a spec that only
    installs the packages we specified in the "conda create" call. Then, we let conda figure
    out which additional packages are required on the specific system. Hopefully this works
    as I expect it.

--- scripts/conda_env.rdkit2019.create.sh
+++ scripts/conda_env.rdkit2019.create.sh
@@ -2,11 +2,30 @@
 
 # I used the following command to create the conda environment rdkit2019
 # Date: Sep 19, 2019
-conda create -n rdkit2019 -c conda-forge -c bioconda -c r -c anaconda python pip tensorflow keras numpy pandas rdkit scipy tensorboard matplotlib scikit-learn seaborn markdown ncurses pcre yaml pyyaml
+conda create -n rdkit2019 -c conda-forge -c bioconda -c r -c anaconda \
+  python \
+  pip \
+  tensorflow \
+  keras \
+  numpy \
+  pandas \
+  rdkit \
+  scipy \
+  tensorboard \
+  matplotlib \
+  scikit-learn \
+  seaborn markdown \
+  ncurses \
+  pcre \
+  yaml \
+  pyyaml \
+  pytest
 
-# I exported this environment to this yaml configuration file wich
-# then includes all installed package releases
-conda env export -n rdkit2019 > conda_env.rdkit2019.yaml
+# Export environment requirements
+# The --from-history ensures that only packages go into the yaml file that we *initially* specified.
+# This should make the script cross-platform since many dependencies that are also installed are
+# system-dependent.
+conda env export -n rdkit2019 --from-history > conda_env.rdkit2019.yml
 
 # To create exactly this environment call
 conda env create -f conda_env.rdkit2019.yml
