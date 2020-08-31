3c44ad31a863f7d3b57adecbda8ab8e92348b7ce
Patrick Scheibe <patrick@halirutan.de>
2020-08-31 11:28:21 +0200

    Re-Add # for batch script

--- scripts/mpcdf-batch-job.sh
+++ scripts/mpcdf-batch-job.sh
@@ -1,27 +1,24 @@
 #!/bin/bash -l
 # Standard output and error:
-SBATCH -o ./tjob.out.%j
-SBATCH -e ./tjob.err.%j
+#SBATCH -o /u/pscheibe/deepFPlearn/tjob.out.%j
+#SBATCH -e /u/pscheibe/deepFPlearn/tjob.err.%j
 # Initial working directory:
-SBATCH -D ./
+#SBATCH -D /u/pscheibe/deepFPlearn
 #
-SBATCH -J dfpl_analysis
+#SBATCH -J dfpl_analysis
 #
 # Node feature:
-SBATCH --constraint="gpu"
+#SBATCH --constraint="gpu"
 # Specify type and number of GPUs to use:
 #   GPU type can be v100 or rtx5000
-SBATCH --gres=gpu:v100:2         # If using both GPUs of a node
-# #SBATCH --gres=gpu:v100:1       # If using only 1 GPU of a shared node
-# #SBATCH --mem=92500             # Memory is necessary if using only 1 GPU
+#SBATCH --gres=gpu:v100:2         # If using both GPUs of a node
 #
 # Number of nodes and MPI tasks per node:
-SBATCH --nodes=1
-SBATCH --ntasks-per-node=40      # If using both GPUs of a node
-# #SBATCH --ntasks-per-node=20    # If using only 1 GPU of a shared node
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40      # If using both GPUs of a node
 #
 #SBATCH --mail-type=none
-#SBATCH --mail-user=<userid>@rzg.mpg.de
+#SBATCH --mail-user=pscheibe@rzg.mpg.de
 #
 # wall clock limit:
 #SBATCH --time=24:00:00
