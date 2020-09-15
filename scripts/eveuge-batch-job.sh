f54cd2f5f31f1ee3c4f7422dd03a85a693882dde
mai00fti <jana.schor@ufz.de>
2020-09-15 15:49:14 +0200

    adjust eve run and submission scripts

--- scripts/eveuge-batch-job.sh
+++ scripts/eveuge-batch-job.sh
@@ -4,9 +4,11 @@
 #$ -S /bin/bash
 
 # requesting resources
-#$ -l gpu=1
 #$ -l h_rt=168:00:00
 #$ -l h_vmem=8G
+#$ -l gpu=1
+#$ -binding linear:1
+
 
 # Standard output and error:
 #$ -o /work/$USER/$JOB_NAME-$JOB_ID.out
@@ -14,7 +16,7 @@
 #$ -j y
 
 # Initial working directory:
-#$ -wd /work/$USER/deepFPlearn
+#$ -wd /home/hertelj/git-hertelj/code/deepFPlearn/
 
 # mail notification (b-egin, e-nd, a-bort)
 #$ -m bea
@@ -22,14 +24,17 @@
 
 module purge
 # use a CUDA-enabled (EasyBuild) toolchain
-module load gcccuda
+#module load gcccuda
 module load fosscuda
 
-module load anaconda/5/5.0.1
-source /global/apps/bioinf/tools/anaconda/5/5.0.1/etc/profile.d/conda.sh
-conda activate rdkit2019
+module load Anaconda2/2019.10 
+source /software/easybuild-broadwell/software/Anaconda2/2019.10/etc/profile.d/conda.sh
+conda activate rdkit2019TF 
+#conda develop -u dfpl
 conda develop dfpl
 
 # Run the program:
 #scripts/run-all-cases.sh > eveuge_dfpl_stdout.txt
-python -m dfpl train -f "validation/case_02/train.json"
+python -m dfpl train -f "/home/hertelj/git-hertelj/code/deepFPlearn/validation/case_02/train.json"
+
+#python /home/hertelj/git-hertelj/code/deepFPlearn/doIgetTheGPU.py
--- scripts/submitTrainingGPU.sh
+++ scripts/submitTrainingGPU.sh
@@ -3,7 +3,7 @@
 #$ -N dfpl_train_gpu
 #$ -S /bin/bash
 #$ -l h_rt=10:00:00
-#$ -l h_vmem=30G	# singularity requires a fair bit of memory
+#$ -l h_vmem=8G	# singularity requires a fair bit of memory
 #$ -l gpu=1	# request GPUs here!
 #$ -binding linear:1
 
@@ -12,13 +12,15 @@
 #$ -e /work/$USER/$JOB_NAME.err
 
 # Train on Sun et al data
-singularity run --nv /global/apps/deepFPlearn/deepfplearn.sif train -i /data/bioinf/projects/data/2019_IDA-chem/deepFPlearn/input/Sun_etal_dataset.csv -o /data/bioinf/projects/data/2019_IDA-chem/deepFPlearn/modeltraining/kfoldCV_GPU/ -t 'smiles' -k 'topological' -s 2048 -d 256 -e 2000 -l 0.2 -K 5 -v 2
+singularity run --nv /global/apps/bioinf/singularity_images/conda_rdkit2019.sif "python -m train -f /home/hertelj/git-hertelj/code/deepFPlearn/validation/case_02/train.json"
+
+#singularity run --nv /global/apps/deepFPlearn/deepfplearn.sif train -i /data/bioinf/projects/data/2019_IDA-chem/deepFPlearn/input/Sun_etal_dataset.csv -o /data/bioinf/projects/data/2019_IDA-chem/deepFPlearn/modeltraining/kfoldCV_GPU/ -t 'smiles' -k 'topological' -s 2048 -d 256 -e 2000 -l 0.2 -K 5 -v 2
 
 # Train on BindingDB data
-singularity run --nv /global/apps/deepFPlearn/deepfplearn-sc.sif train -i /data/bioinf/projects/data/2020_deepFPlearn/dataSources/BindingDB/data/07_BindingDB.trainingSet.csv -o /data/bioinf/projects/data/2019_IDA-chem/deepFPlearn/modeltraining/kfoldCV_GPU/ -t 'smiles' -k 'topological' -s 2048 -d 256 -e 2000 -l 0.2 -K 5 -v 2
+#singularity run --nv /global/apps/deepFPlearn/deepfplearn-sc.sif train -i /data/bioinf/projects/data/2020_deepFPlearn/dataSources/BindingDB/data/07_BindingDB.trainingSet.csv -o /data/bioinf/projects/data/2019_IDA-chem/deepFPlearn/modeltraining/kfoldCV_GPU/ -t 'smiles' -k 'topological' -s 2048 -d 256 -e 2000 -l 0.2 -K 5 -v 2
 
 # Train on combinded Sun-BindingDB data set
-singularity run --nv /global/apps/deepFPlearn/deepfplearn-sc.sif train -i /data/bioinf/projects/data/2020_deepFPlearn/dataSources/combinedBDBSund/01_combinedSUN-BDB.dataset.4training.csv -o /data/bioinf/projects/data/2019_IDA-chem/deepFPlearn/modeltraining/kfoldCV_GPU/combinedBDBSUN/ -t 'smiles' -k 'topological' -s 2048 -d 256 -e 2000 -l 0.2 -K 5 -v 2
+#singularity run --nv /global/apps/deepFPlearn/deepfplearn-sc.sif train -i /data/bioinf/projects/data/2020_deepFPlearn/dataSources/combinedBDBSund/01_combinedSUN-BDB.dataset.4training.csv -o /data/bioinf/projects/data/2019_IDA-chem/deepFPlearn/modeltraining/kfoldCV_GPU/combinedBDBSUN/ -t 'smiles' -k 'topological' -s 2048 -d 256 -e 2000 -l 0.2 -K 5 -v 2
 
 #Your job 6656657 ("dfpl_train_gpu_AC") has been submitted
 # 13.07.2020 15:30
