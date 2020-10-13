027df440f4a888616ceda030166fb38fec13b200
Patrick Scheibe <patrick@halirutan.de>
2020-10-13 07:23:13 +0200

    Remove umapping from master branch until it is ready

--- scripts/run-all-publication-cases.sh
+++ scripts/run-all-publication-cases.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # This script needs to be run from the deepFPlearn directory.
-# Importantly, the conda environment needs to be set up and actived! For certain machines/HPC,
+# Importantly, the conda environment needs to be set up and activated! For certain machines/HPC,
 # we have a batch-job that does exactly that and then calls this file
 
 function log_error() {
