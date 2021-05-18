39f8d1c4a9e11d8670719829a0edcecd2611df12
Jana Schor <jana.schor@ufz.de>
2021-05-18 16:15:35 +0200

    minor changes in main.py

--- scripts/run-feasible-MoleculeNet-cases.sh
+++ scripts/run-feasible-MoleculeNet-cases.sh
@@ -4,7 +4,7 @@
 # Importantly, the conda environment needs to be set up and actived! For certain machines/HPC,
 # we have a batch-job that does exactly that and then calls this file
 
-D="data/"; if [ -d $D ]; then python -m dfpl convert -f $D; fi
+#D="data/"; if [ -d $D ]; then python -m dfpl convert -f $D; fi
 #D="data/MoleculeNet/Biophysics/"; if [ -d $D ]; then python -m dfpl convert -f $D; fi
 
 #F="/home/hertelj/git-hertelj/code/deepFPlearn/validation/case_MUV/train_AC-generic.json"; if [ -f $F ]; then python -m dfpl train -f $F; fi
