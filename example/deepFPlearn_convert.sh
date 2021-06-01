e114462194138a03df0802c2efead2ec76c32e7a
Jana Schor <jana.schor@ufz.de>
2021-06-01 17:39:55 +0200

    adjust paths in example files

--- example/deepFPlearn_convert.sh
+++ example/deepFPlearn_convert.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-D="example/data/"
+script_dir=$(dirname "$0")
+D="$script_dir/data/"
 
 # check if conda env exists, create if not, activate it
 env=$(conda env list | grep 'dfpl_env' | wc -l)
 if [[ $env -ne 1 ]]; then
-  conda env create -f ../singularity_container/environment.yml
+  conda env create -f "$script_dir/../singularity_container/environment.yml"
 fi
 source activate dfpl_env
-cd ..
 
 if [ -d $D ]; then
+  export PYTHONPATH="$script_dir/.."
   python -m dfpl convert -f $D
 fi
