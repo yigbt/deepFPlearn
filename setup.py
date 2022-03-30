a21b07fea65bf79b50ecc5bace022225e3522c95
Matthias Bernt <m.bernt@ufz.de>
2022-03-30 11:00:16 +0200

    update setup.py: add entrypoint
    - explicitly list packages
    - add requirements (for pip installation)

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version="0.1",
     author="Jana Schor, Patrick Scheibe",
     author_email="jana.schor@ufz.de",
-    packages=find_packages(),
+    packages=find_packages(include=['dfpl', 'dfpl.*']),
     long_description=readme_text,
     long_description_content_type="text/markdown",
     url="https://github.com/yigbt/deepFPlearn",
@@ -16,5 +16,17 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent"
     ],
-    python_requires='>=3.6'
+    python_requires='>=3.6',
+    install_requires=[
+        "jsonpickle",
+        "pandas",
+        "rdkit-pypi",
+        "keras",
+        "tensorflow",
+        "scikit-learn",
+        "matplotlib",
+    ],
+    entry_points={
+        'console_scripts': ['dfpl=dfpl.__main__:main']
+    }
 )
