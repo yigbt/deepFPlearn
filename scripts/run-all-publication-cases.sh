a8625a0559bb782026055f0932dc037f7d43c3c3
Patrick Scheibe <patrick@halirutan.de>
2022-04-11 22:05:07 +0200

    Finalize, hopefully, the train and predict configurations

--- scripts/run-all-publication-cases.sh
+++ scripts/run-all-publication-cases.sh
@@ -32,10 +32,12 @@ function call_predict() {
   fi
 }
 
+#call_train "deepFPlearn_TRAINING/case_00/train_AE_D.json"
+#call_train "deepFPlearn_TRAINING/case_00/train_AE_D_full.json"
 call_train "deepFPlearn_TRAINING/case_00/train_AE_S.json"
-call_train "deepFPlearn_TRAINING/case_00/train_AE_D.json"
-call_train "deepFPlearn_TRAINING/case_00/train_AE_Sider.json"
-call_train "deepFPlearn_TRAINING/case_00/train_AE_Tox21.json"
+call_train "deepFPlearn_TRAINING/case_00/train_AE_S_full.json"
+call_train "deepFPlearn_TRAINING/case_00/train_AE_Sider_full.json"
+call_train "deepFPlearn_TRAINING/case_00/train_AE_Tox21_full.json"
 
 call_train "deepFPlearn_TRAINING/case_01/train.json"
 
@@ -44,11 +46,25 @@ call_train "deepFPlearn_TRAINING/case_02/train.json"
 call_train "deepFPlearn_TRAINING/case_03/train.json"
 
 call_predict "deepFPlearn_TRAINING/case_04/predict_D-AR.json"
-call_predict "deepFPlearn_TRAINING/case_04/predict_D-ED.json"
 call_predict "deepFPlearn_TRAINING/case_04/predict_D-ER.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D-ED.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D-GR.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D-TR.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D-PPARg.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D-Aromatase.json"
 
+call_predict "deepFPlearn_TRAINING/case_04/predict_D_uncompressed-AR.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D_uncompressed-ER.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D_uncompressed-ED.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D_uncompressed-GR.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D_uncompressed-TR.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D_uncompressed-PPARg.json"
+call_predict "deepFPlearn_TRAINING/case_04/predict_D_uncompressed-Aromatase.json"
+
+call_train "deepFPlearn_TRAINING/case_sider/train_SIDER.json"
 call_train "deepFPlearn_TRAINING/case_sider/train_SIDER-AE_D.json"
 call_train "deepFPlearn_TRAINING/case_sider/train_SIDER-AE_Sider.json"
 
+call_train "deepFPlearn_TRAINING/case_tox21/train_Tox21.json"
 call_train "deepFPlearn_TRAINING/case_tox21/train_Tox21-AE_D.json"
 call_train "deepFPlearn_TRAINING/case_tox21/train_Tox21-AE_Tox21.json"
