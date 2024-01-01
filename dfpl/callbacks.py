e376b7eac6912de3773cf4589e5c7c1fa45c42d5
soulios <kyriakos.soulios@ufz.de>
2024-01-01 20:51:55 +0100

    update options and saving for encoders

--- dfpl/callbacks.py
+++ dfpl/callbacks.py
@@ -22,28 +22,37 @@ def autoencoder_callback(checkpoint_path: str, opts: options.Options) -> list:
     else:
         target = "loss"
         # enable this checkpoint to restore the weights of the best performing model
-    checkpoint = ModelCheckpoint(
+    if opts.aeType == "deterministic":
+        checkpoint = ModelCheckpoint(
         checkpoint_path,
         monitor=target,
         mode="min",
         verbose=1,
-        period=settings.ac_train_check_period,
+        save_freq="epoch",
         save_best_only=True,
-        save_weights_only=True,
-    )
+        )
+    else:
+        checkpoint = ModelCheckpoint(
+        checkpoint_path,
+        monitor=target,
+        mode="min",
+        verbose=1,
+        save_freq="epoch",
+        save_best_only=True,
+        save_weights_only=True
+        )
     callbacks.append(checkpoint)
 
     # enable early stopping if val_loss is not improving anymore
     early_stop = EarlyStopping(
-        monitor=target,
-        mode="min",
-        patience=settings.ac_train_patience,
-        min_delta=settings.ac_train_min_delta,
-        verbose=1,
-        restore_best_weights=True,
+    monitor=target,
+    mode="min",
+    patience=settings.ac_train_patience,
+    min_delta=settings.ac_train_min_delta,
+    verbose=1,
+    restore_best_weights=True,
     )
     callbacks.append(early_stop)
-
     if opts.aeWabTracking and not opts.wabTracking:
         callbacks.append(WandbCallback(save_model=False))
     return callbacks
@@ -65,7 +74,7 @@ def nn_callback(checkpoint_path: str, opts: options.Options) -> list:
         checkpoint = ModelCheckpoint(
             checkpoint_path,
             verbose=1,
-            period=settings.nn_train_check_period,
+            save_freq="epoch",
             save_best_only=True,
             monitor="val_loss",
             mode="min",
