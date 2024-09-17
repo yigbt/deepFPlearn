6f8f6adfcc95bbe3b7a393142a9f2804e4a8ea8c
dil-shana <dilshana.shanavas-rasheeda@ufz.de>
2024-09-17 14:49:42 +0200

    latest fits

--- example/predict/AR/comp_norm/plot_ar_vs_predicted.R
+++ example/predict/AR/comp_norm/plot_ar_vs_predicted.R
@@ -9,12 +9,12 @@ data <- read.csv("predict_data_AR.csv")
 ggplot(data, aes(x = AR, y = predicted)) +
   geom_point(color = 'blue', alpha = 0.6) +  # Adds points with some transparency
   geom_abline(intercept = 0, slope = 1, color = 'red', linetype = "dashed") +  # Adds the y=x reference line
-  labs(title = "Comparison of AR values and Predicted values",
-       x = "AR values (Real)",
+  labs(
+       x = "Actual Values",
        y = "Predicted values") +
   theme_minimal() +
   theme(plot.title = element_text(hjust = 0.5))
 
 # Save the plot as a PNG file
-ggsave("ar_vs_predicted_comparison_plot.png")
+ggsave("ar_comp.png")
 
